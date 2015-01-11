211217d534c29ff24f54fe1a5eece585ec8a0793
stevenmarr <stevenmarr@me.com>
2015-01-10 23:42:35 -0700

    Add show_trailer method to Movie class

--- entertainment_center.py
+++ entertainment_center.py
@@ -3,6 +3,14 @@ import media
 toy_story = media.Movie("Toy Story",
                         "A story of a boy and his awesome toys",
                         "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
-                        "https://www.youtube.com/watch?v-vwyZH85NQC4")
+                        "https://www.youtube.com/watch?v=KYz2wyBy3kc")
 
 print toy_story.storyline
+
+avatar = media.Movie("Avatar",
+                     "A marine on an alien planet")
+
+print avatar.storyline
+
+#avatar.show_trailer()
+toy_story.show_trailer()
--- media.py
+++ media.py
@@ -1,6 +1,16 @@
+import webbrowser
+
 class Movie():
-    def __init__(self,movie_title, movie_storyline = None, poster_image = None, trailer_youtube = None ):
+    def __init__(self,movie_title,
+                movie_storyline = None,
+                poster_image = None,
+                trailer_youtube = None ):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
+    def show_trailer(self):
+        try:
+            webbrowser.open(self.trailer_youtube_url)
+        except:
+            webbrowser.open("https://www.google.com/search?q=youtube+ %s +trailer" % self.title)
