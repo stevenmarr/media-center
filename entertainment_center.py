f24e0b4531ad0350ce285d3c226784f8f8d1a5dc
stevenmarr <stevenmarr@me.com>
2015-01-11 10:34:31 -0700

    Integrate fresh_tomatoes allows for html rendering and creation

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,4 +1,6 @@
 import media
+import fresh_tomatoes
+
 
 toy_story = media.Movie("Toy Story",
                         "A story of a boy and his awesome toys",
@@ -8,9 +10,14 @@ toy_story = media.Movie("Toy Story",
 print toy_story.storyline
 
 avatar = media.Movie("Avatar",
-                     "A marine on an alien planet")
+                     "A marine on an alien planet","",
+                     "www.youtube.com/watch?v=cRdxXPV9GNQ")
 
-print avatar.storyline
+mystery_alaska = media.Movie("Mystery Alaska",
+                            "This comedy is about the residents of a small town who get over-excited when their hockey team gets chosen to host a televised event",
+                            "http://ia.media-imdb.com/images/M/MV5BMjE2MDM2NzcyMl5BMl5BanBnXkFtZTYwMzA2NjQ3._V1_SX300.jpg",
+                            "www.youtube.com/watch?v=a80x06Wn91U")
 
-#avatar.show_trailer()
-toy_story.show_trailer()
+movies = [toy_story, avatar, mystery_alaska]
+
+fresh_tomatoes.open_movies_page(movies)
