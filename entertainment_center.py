ab65b84dd323a9973eaced1e57670701afae8dc8
stevenmarr <stevenmarr@me.com>
2015-01-11 11:40:17 -0700

    Query http://www.omdbapi.com/ API for plot and poster URL when not specified on movie creation

--- entertainment_center.py
+++ entertainment_center.py
@@ -3,21 +3,29 @@ import fresh_tomatoes
 
 
 toy_story = media.Movie("Toy Story",
-                        "A story of a boy and his awesome toys",
-                        "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
                         "https://www.youtube.com/watch?v=KYz2wyBy3kc")
 
-print toy_story.storyline
-
 avatar = media.Movie("Avatar",
-                     "A marine on an alien planet","",
                      "www.youtube.com/watch?v=cRdxXPV9GNQ")
 
 mystery_alaska = media.Movie("Mystery Alaska",
-                            "This comedy is about the residents of a small town who get over-excited when their hockey team gets chosen to host a televised event",
-                            "http://ia.media-imdb.com/images/M/MV5BMjE2MDM2NzcyMl5BMl5BanBnXkFtZTYwMzA2NjQ3._V1_SX300.jpg",
                             "www.youtube.com/watch?v=a80x06Wn91U")
 
-movies = [toy_story, avatar, mystery_alaska]
+the_princess_bride = media.Movie("The Princess Bride",
+                                "https://www.youtube.com/watch?v=njZBYfNpWoE")
+clue = media.Movie("Clue",
+                    "https://www.youtube.com/watch?v=NHEpuz_gUGM")
+despicable_me = media.Movie("Despicable Me",
+                    "https://www.youtube.com/watch?v=sUkZFetWYY0")
+
+
+
+
+
+
+
+
+movies = [toy_story, avatar, mystery_alaska, the_princess_bride, clue,
+            despicable_me]
 
 fresh_tomatoes.open_movies_page(movies)
--- media.py
+++ media.py
@@ -4,23 +4,29 @@ import urllib
 
 class Movie():
     def __init__(self,movie_title,
+                trailer_youtube,
                 movie_storyline = None,
-                poster_image = None,
-                trailer_youtube = None ):
+                poster_image = None):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
-        if self.storyline == None:
-            url = ("http://www.omdbapi.com/?t= %s &y=&plot=short&r=json" % self.title)
-            response = urlopen(url)
-
-            string = response.read().decode('utf-8')
-            json_obj = json.loads(string)
-
-            self.storyline = json_obj['Plot']
+        if (self.storyline == None or
+            self.poster_image_url == None):
+            try:
+                url = ("http://www.omdbapi.com/?t=%s&y=&plot=short&r=json"%self.title)
+                response = urllib.urlopen(url)
+                string = response.read().decode('utf-8')
+                json_obj = json.loads(string)
+                if self.storyline == None:
+                    self.storyline = json_obj['Plot']
+                if self.poster_image_url == None:
+                    self.poster_image_url = json_obj['Poster']
+            except:
+                self.storyline = "No plot information available"
+                self.poster_image_url = "http://uploads.neatorama.com/images/posts/95/58/58095/1360112719-0.jpg"
     def show_trailer(self):
         try:
             webbrowser.open(self.trailer_youtube_url)
         except:
-            webbrowser.open("https://www.google.com/search?q=youtube+ %s +trailer" % self.title)
+            webbrowser.open("https://www.google.com/search?q=youtube+%s+trailer" %self.title)
