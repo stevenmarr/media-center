ab9400e931f1d58a473e74812a5fefba0f445cb5
Steven Marr(w) <Work@Stevens-MacBook-Pro.local>
2015-03-12 21:03:13 -0600

    Add comments

--- media.py
+++ media.py
@@ -7,6 +7,16 @@ class Movie():
                 trailer_youtube,
                 movie_storyline = None,
                 poster_image = None):
+     """Creates a new movie object for viewing trailer, story line, and a poster image.
+    
+    Args:
+      movie_title: The title of the movie to add.
+      trailer_youtube: URL of a version of the moview trailers found on YouTube.com
+      movie_storyline: a string of the movies description, if left unnassigned the app 
+      will query the omdapi to search for movie description
+      poster_image: A url where the movies poster artwork can be found, if left blank the 
+      app will query the omdapi to search for the poster image
+    """
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
@@ -26,7 +36,8 @@ class Movie():
                 self.storyline = "No plot information available"
                 self.poster_image_url = "http://uploads.neatorama.com/images/posts/95/58/58095/1360112719-0.jpg"
     def show_trailer(self):
+        """Open window in webbrowser to view trailer"""
         try:
             webbrowser.open(self.trailer_youtube_url)
         except:
-            webbrowser.open("https://www.google.com/search?q=youtube+%s+trailer" %self.title)
+            webbrowser.open("https://www.google.com/search?q=youtube+%s+trailer" % self.title)
